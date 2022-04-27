07131eb45e4cd5da45d293ce937a9ffacafdd21b
Yuto Shimizu <yuto.shimizu.19@gmail.com>
2022-04-27 21:22:43 +0900

    末吉と凶です

---
+++ omikuji.js
@@ -0,0 +1,26 @@
+function omikuji() {
+  let omikujiList = new Array(
+    "daikichi",
+    "kichi",
+    "chukichi",
+    "syoukichi",
+    "suekichi",
+    "kyou"
+  );
+  let languageList = new Array(
+    "C",
+    "C++",
+    "C",
+    "Java",
+    "JavaScript",
+    "Python",
+    "Ruby",
+    "Swift",
+    "Go",
+    "PHP"
+  );
+  let dbList = new Array("MySQL", "PostgreSQL", "SQLite", "Oracle", "DB2");
+  let editor = new Array("VScode", "Atom", "Brackets", "Vim", "Terapad");
+
+  let random = Math.random();
+}
